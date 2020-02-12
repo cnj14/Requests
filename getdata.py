ab7d449b9a23da4753b73072267c56e94640ecb9
cnj14 <59627768+cnj14@users.noreply.github.com>
2020-02-12 17:42:00 -0500

    Get data

--- getdata.py
+++ getdata.py
@@ -1,3 +1,18 @@
 # getdata.py
 
+import requests
+
 print("GETTING SOME DATA FROM THE INTERNET...")
+
+request_url = "https://raw.githubusercontent.com/prof-rossetti/intro-to-python/master/data/products/1.json"
+print("URL: ", request_url)
+
+response = requests.get(request_url)
+print(type(response))
+# print(dir(response))
+
+print(response.status_code)
+print(response.text)
+
+breakpoint()
+
